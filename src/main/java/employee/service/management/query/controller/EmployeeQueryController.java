a663eb7a436926250fb89bad27dd6bddc89d6e07
Omid <o.zare70@gmail.com>
2023-10-16 12:25:02 +0200

    master

--- src/main/java/employee/service/management/query/controller/EmployeeQueryController.java
+++ src/main/java/employee/service/management/query/controller/EmployeeQueryController.java
@@ -17,7 +17,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.zalando.problem.Problem;
 
 @RestController
 @RequiredArgsConstructor
